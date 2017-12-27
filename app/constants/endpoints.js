23f0596eb6044aea91f60cbaff2c53b6ff218fbc
David Vanegas <davidv@alquemedia.com>
2017-12-26 19:37:18 -0500

    change constants

--- app/constants/endpoints.js
+++ app/constants/endpoints.js
@@ -7,15 +7,15 @@ const endpoints = {
 
 export const route = 'works';
 
-/*
+
 export const firebaseConfig = {
   apiKey: 'AIzaSyBzTmfOo2q3zEfh7krPEorQZA-9rDP6sCo',
   authDomain: 'devhack-55d18.firebaseapp.com',
   databaseURL: 'https://devhack-55d18.firebaseio.com',
   storageBucket: 'devhack-55d18.appspot.com',
 };
-*/
 
+/*
 export const firebaseConfig = {
   apiKey: 'AIzaSyDfi0rFpj2z_IVIk4ZPn5WM0HF7JmInviw',
   authDomain: 'ascodevida-51dc0.firebaseapp.com',
@@ -24,5 +24,5 @@ export const firebaseConfig = {
   storageBucket: 'ascodevida-51dc0.appspot.com',
   messagingSenderId: '1017450578124',
 };
-
+*/
 export default endpoints;
